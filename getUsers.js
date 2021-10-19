b4e4dc25645a618389d6d6117341cb68d10827fa
abdelrahimZak <abdelrahim.zakaria.first@gmail.com>
2021-10-19 11:33:32 +0300

    add bootstrap

--- getUsers.js
+++ getUsers.js
@@ -1,13 +1,12 @@
 
-document.getElementById('submit').onclick=get;
-function get(){
-    var email = document.getElementById("email").value;
+
+
+   
     
-    var url=(email=="")?"https://ancient-basin-17410.herokuapp.com/users/":
-    "https://ancient-basin-17410.herokuapp.com/getUser?email="+email;
+    var url="https://ancient-basin-17410.herokuapp.com/users";
+    
 
     var table= document.getElementById("table");
-    table.innerHTML="<tr><th>email</th><th>password</th></tr>";
     
     fetch(url)
     .then(response => response.json())
@@ -15,16 +14,58 @@ function get(){
         var jsondata=[];
      jsondata=jsondata.concat( data); 
      jsondata.forEach(user => {
-            
+         
+            // creat tabel row
           var tr = document.createElement("tr");
+          tr.setAttribute('class',"alert");
+          tr.setAttribute('role',"alert");
+
+          // table data
+
+          // check box
+          var td_checkBox=document.createElement("td");
+           td_checkBox.innerHTML='<label class="checkbox-wrap checkbox-primary"><input type="checkbox" checked><span class="checkmark"></span></label>';
+          //email          
           var td_email = document.createElement("td");
-          var td_password = document.createElement("td");
-          tr.append(td_email,td_password);
+          td_email.setAttribute('class',"d-flex align-items-center");
+          td_email.innerHTML='<div class="img" style="background-image: url(\'https://cdn.pixabay.com/photo/2018/08/28/13/29/avatar-3637561_960_720.png\');"></div>'+
+         '<div class="pl-3 email">'+
+         '<span>'+user.email+'</span>'+
+         '<span>Added: 01/10/2021</span></div>';
+          
+
+          var td_name = document.createElement("td");
+          
+          var nameMatch = user.email.match(/^([^@]*)@/);
+          var Uname = nameMatch ? nameMatch[1] : user.email;
+          if(Uname.includes('.')){
+          Uname=Uname.split('.').join(' ');
+          }
+          td_name.innerHTML=Uname;
+          tr.append(td_checkBox,td_email,td_name);
+          
           table.append(tr);
-          td_email.innerHTML=user.email;
-          td_password.innerHTML=user.password;
+          
+          
 
         });
     });
 
-}
+/*
+
+						    
+						    
+						      	
+						      	
+						      		
+						      		
+						      </td>
+						      <td>Markotto89</td>
+						      <td class="status"><span class="active">Active</span></td>
+						      <td>
+						      	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+				            	<span aria-hidden="true"><i class="fa fa-close"></i></span>
+				          	</button>
+				        	</td>
+						    </tr>
+*/
--- login.js
+++ login.js
@@ -9,6 +9,50 @@ var password= document.getElementById("password").value;
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
+var raw = JSON.stringify([
+  {
+    "email":email,
+    "password":password
+  }
+]);
+
+var requestOptions = {
+  method: 'POST',
+  headers: myHeaders,
+  body: raw,
+  redirect: 'follow'
+};
+
+
+fetch("https://ancient-basin-17410.herokuapp.com/Authenticate", requestOptions)
+  .then(response => response.json())
+  .then(result =>{
+  if(result)
+  {
+    window.alert("welcome");
+    document.location.href="welcomePage.html?name="+email;
+   
+    
+  }
+  else{
+    window.alert("wrong");
+  }
+    
+  } )
+ .catch(error => console.log('error', error));
+
+
+}
+document.getElementById("insert").onclick=insert;
+
+function insert(){
+var email = document.getElementById("email").value;
+var password= document.getElementById("password").value;
+
+
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
 var raw = JSON.stringify([{
     "email":email,
     "password":password
@@ -21,10 +65,10 @@ var requestOptions = {
   redirect: 'follow'
 };
 
-fetch("https://ancient-basin-17410.herokuapp.com/Authenticate", requestOptions)
-  .then(response => response.json())
+fetch("https://ancient-basin-17410.herokuapp.com/AddUser", requestOptions)
+  .then(response => response.text())
   .then(result =>{
-   window.alert((result)?"welcom":"wrong password or user email");
+   window.alert(result);
 location.reload();
     
   } )
